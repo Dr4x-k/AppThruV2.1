af567d52a2a35f59f026d0d5660c04dc2bfcc049
Dr4x-k <jabe.4124@gmail.com>
2022-10-15 13:18:35 -0700

    Vista de Registro - Bernal

---
+++ src/public/js/index.js
@@ -0,0 +1,72 @@
+const form = document.querySelector("form"),
+    emailField = form.querySelector(".email-field"),
+    emailInput = emailField.querySelector(".email"),
+    pswField = form.querySelector(".password-field"),
+    pswInput = pswField.querySelector(".psw"),
+    cpswField = form.querySelector(".cPassword-field"),
+    cpswInput = cpswField.querySelector(".cpsw");
+
+// Email validator
+function checkEmail() {
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailInput.value.match(emailPattern) && emailInput.value !== "") {
+        return emailField.classList.add("invalid");
+    }
+    emailField.classList.remove("invalid");
+    
+}
+
+//
+
+const eyeIcons = document.querySelectorAll(".show-hide");
+//console.log(eyeIcons);
+
+eyeIcons.forEach(eyeIcons => {
+    eyeIcons.addEventListener("click", () => {
+        const pInput = eyeIcons.parentElement.querySelector("input");
+        //console.log(pInput);
+        //
+        if (pInput.type === "password") {
+            eyeIcons.classList.replace("bx-hide", "bx-show");
+            return (pInput.type = "text");
+        }
+        eyeIcons.classList.replace("bx-show", "bx-hide");
+        pInput.type = "password";
+    });
+});
+
+// Password Validator
+function createPass() {
+    const passPatern =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!pswInput.value.match(passPatern) && pswInput.value !== "") {
+        return pswField.classList.add("invalid");
+    }
+    pswField.classList.remove("invalid");
+}
+
+// Confirm password validation
+function confirmPass() {
+    if (pswInput.value !== cpswInput.value && cpswInput.value !== "") {
+        console.log(true);
+        return cpswField.classList.add("invalid");
+    }
+    cpswField.classList.remove("invalid");
+}
+
+// call funciton checkEmail
+form.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    checkEmail();
+    createPass();
+    confirmPass();
+
+    //if (emailInput.value !== "") emailInput.addEventListener("keyup", checkEmail);
+    //if (pswInput.value !== "") pswInput.addEventListener("keyup", createPass);
+    //if (cpswInput.value !== "") cpswInput.addEventListener("keyup", confirmPass);
+    
+
+    /* if (!emailField.classList.contains("invalid") && !pswField.classList.contains("invalid") && !cpswField.classList.contains("invalid")) {
+        location.href = form.getAttribute("action")
+    } */
+});
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,4 +10,12 @@ routes.get('/login', (req, res) => {
     res.render('login');
 });
 
+routes.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+routes.get('/account', (req, res) => {
+    res.render('accountSettings');
+});
+
 module.exports = routes;
