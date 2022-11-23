e1278d2d2f9ef8ce9dd54ac08a0582d1c2bcebaf
Dr4x-k <jabe.4124@gmail.com>
2022-11-23 11:13:20 -0700

    nose

---
+++ sql/vistas.sql
@@ -0,0 +1,20 @@
+use abarrotes;
+
+create view viewUsuarios as
+select us.idUsuario, us.nombres, us.apellidoPaterno,
+    us.apellidoMaterno, us.usuario,
+    r.nombre as rol
+from usuario as us
+inner join rol as r
+on r.idRol = us.fk_rol;
+
+create view viewProductos as
+select prod.idProducto, prod.nombreProducto,
+    prod.descripcion, prod.precio, cat.categoria
+from productos as prod
+inner join categoria as cat
+on cat.idCategoria = prod.fk_idCategoria;
+
+select * from viewProductos;
+
+select * from viewUsuarios
